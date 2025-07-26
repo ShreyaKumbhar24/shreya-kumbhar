c73c04681ecd4c0eead80b0772c5a8912194b320
Shreya <shreyaaa024@gmail.com>
2025-07-25 19:11:11 -0500

    Updates

---
+++ precache-manifest.3d956c146e8841b0945b878ad7f6be46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe7d5f63bc411c70789a046219667df",
+    "url": "/shreya-kumbhar/index.html"
+  },
+  {
+    "revision": "994fb65c04c8596c6763",
+    "url": "/shreya-kumbhar/static/css/2.13cfa5dc.chunk.css"
+  },
+  {
+    "revision": "e22a463dffa48e7a8ed8",
+    "url": "/shreya-kumbhar/static/css/main.0280e3b9.chunk.css"
+  },
+  {
+    "revision": "994fb65c04c8596c6763",
+    "url": "/shreya-kumbhar/static/js/2.b4f65f57.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/shreya-kumbhar/static/js/2.b4f65f57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e22a463dffa48e7a8ed8",
+    "url": "/shreya-kumbhar/static/js/main.bcf64355.chunk.js"
+  },
+  {
+    "revision": "488462cc20a7cd7017ef",
+    "url": "/shreya-kumbhar/static/js/runtime-main.c590ef88.js"
+  }
+]);
